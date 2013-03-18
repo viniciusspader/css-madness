35bfb098b17238cf696a96519cedc5565071c4f7
unknown <vinicius@pc-vinicius.dakota.com.br>
2013-03-18 08:53:39 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssMadness::Application.config.secret_token = '15a58f0aad9ccb58d6450f09aac6dafa60b753ea01aaa5d42a0e2eb7392500b7a86aa83eacb0b5ac8c33607d05ca75baf25c34845a3b9137acc3e38f5bfb4565'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CssMadness::Application.config.session_store :cookie_store, key: '_css-madness_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CssMadness::Application.config.session_store :active_record_store
